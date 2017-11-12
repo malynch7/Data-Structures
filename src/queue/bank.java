b7ab7585bc1b8f9c6c6883b07c5898dfd8d97602
malynch7 <malynch7@gmail.com>
2017-11-11 21:41:39 -0500

    Cleaned syntax

--- src/queue/bank.java
+++ src/queue/bank.java
@@ -26,6 +26,7 @@ public class bank {
 				tellers[i] = new teller();
 				line.add(new customer(currentTime));
 			}
+			
 			System.out.print("\nBeginning simulation...\n\n120s remaining\n");
 			while((elapsedTime = (currentTime = System.nanoTime()) - startTime)/1000000000 < 120) {
 				if (elapsedTime / 10 % (1000000000)  == 0)
@@ -43,15 +44,16 @@ public class bank {
 					}else{
 						if (currentTime >= tellers[i].nextAvailable) 
 							tellers[i].available = true;
+					}
 				}
 			}
-			System.out.println("\n\nSimulation complete. \n\nTotal number of customers: " + numberOfCustomers + "\nAverage wait time: " + (totalWaitTime / (numberOfCustomers - 5) / 1000000000 ) + "s");
+			System.out.println("\n\nSimulation complete. \n\nTotal number of customers: " + numberOfCustomers);
+			System.out.println("Average wait time: " + (totalWaitTime / (numberOfCustomers - 5) / 1000000000 ) + "s");
 			for (int i = 0; i < 5; i++) 
 				System.out.println("teller " + (i + 1) + " helped " + tellers[i].customersHelped + " customers, and was occupied for " + tellers[i].timeOccupied + "s.");
 			System.out.println(line.size() + " customers did not see a teller.\n");
 			System.out.println("Restart simulation (y/n)?");
 			restart = input.next();
-			}
 		}
 	}
 }
