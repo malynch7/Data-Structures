6b5fecc90a74c9964c904340b165c8d961be1780
malynch7 <malynch7@gmail.com>
2017-11-12 11:40:06 -0500

    Added documentation

--- src/queue/customer.java
+++ src/queue/customer.java
@@ -1,17 +1,27 @@
+//For primary documentation, see bank.java.
 package queue;
 
 import java.util.*;
 
 public class customer {
-
+//This class represents a customer object for use with bank.java.
+	
 	int helpTime;
 	long startTime;
 	long waitTime;
 	Random r = new Random();
 	
+	public customer() {
+	//default constructor
+		startTime = System.nanoTime();
+		helpTime = r.nextInt(5) + 2; 
+	}
+	
 	public customer(long currentTime) {
+	/* Constructor requiring a valid current time.
+	 * Pre: currentTime must be initialized.
+	 * Post: customer contains start time and help time*/
 		startTime = currentTime;
-		
 		helpTime = r.nextInt(5) + 2; 
 	}
 	
--- src/queue/teller.java
+++ src/queue/teller.java
@@ -1,9 +1,9 @@
+//For primary documentation, see bank.java.
 package queue;
 
-import java.util.concurrent.TimeUnit;
-
 public class teller {
-
+//This class represents a teller object for use with bank.java, capable of interacting with a customer object.
+	
 	boolean available;
 	int customersHelped;
 	int timeOccupied = 0;
@@ -11,11 +11,15 @@ public class teller {
 	
 	
 	public teller() {
+	//default constructor	
 		available = true;
 		customersHelped = 0;
 	}
 	
 	public long help(customer customer, long currentTime) {
+	/* Sets availability to false, sets next available time, and returns the customer's wait time.
+	 * Pre: customer and currentTime must be initialized.
+	 * Post: teller's availability is set to false and next available is increased.*/
 		available = false;
 		timeOccupied += customer.helpTime;
 		customer.waitTime = currentTime - customer.startTime; 
