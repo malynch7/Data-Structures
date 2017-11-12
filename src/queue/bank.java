90f0c99cc4d9c9ce3f3c8b75520e44f55ca4df8b
malynch7 <malynch7@gmail.com>
2017-11-11 19:26:53 -0500

    working, reverted from test

--- src/queue/bank.java
+++ src/queue/bank.java
@@ -6,7 +6,7 @@ public class bank {
 
 	public static void main(String[] args) {
 		
-		String restart = " ";
+		String restart = "y";
 		Scanner input = new Scanner(System.in);
 		while (!restart.equals("n")) {
 			int numberOfCustomers = 5;
